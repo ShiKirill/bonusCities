3528681cc9a9a692ee30ea333669085e202a18a6
ShiKirill <shiraevkirill@gmail.com>
2021-03-13 03:26:52 +0700

    Part 2

--- inputCities/db_cities.js
+++ inputCities/db_cities.js
@@ -1,3 +1,4 @@
+'use strict';
 const data = {
     "RU": [
         {
@@ -300,31 +301,189 @@ let totalLine = document.querySelectorAll('.dropdown-lists__total-line');
 const closeBtn = document.querySelector('.close-button');
 const button = document.querySelector('.button');
 const allDropdowns = document.querySelectorAll('.dropdown-lists__list');
+const dropdownBlock = document.querySelector('.dropdown');
 const label = document.querySelector('.label');
 
+document.querySelector('.input-cities').style.opacity = 0;
 
-    const disableButton = (event) => {
-        event.preventDefault(); 
-    };
+const style = document.createElement('style');
+style.textContent = `
+.sk-circle-bounce {
+    width: 4em;
+    height: 4em;
+    position: relative;
+    margin: auto;
+}
+.sk-circle-bounce .sk-child {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    left: 0;
+    top: 0;
+}
+.sk-circle-bounce .sk-child:before {
+    content: "";
+    display: block;
+    margin: 0 auto;
+    width: 15%;
+    height: 15%;
+    background-color: #337ab7;
+    border-radius: 100%;
+    -webkit-animation: sk-circle-bounce-delay 1.2s infinite ease-in-out both;
+    animation: sk-circle-bounce-delay 1.2s infinite ease-in-out both;
+}
+.sk-circle-bounce .sk-circle-2 {
+    transform: rotate(30deg);
+}
+.sk-circle-bounce .sk-circle-2:before {
+    -webkit-animation-delay: -1.1s;
+    animation-delay: -1.1s;
+}
+.sk-circle-bounce .sk-circle-3 {
+    transform: rotate(60deg);
+}
+.sk-circle-bounce .sk-circle-3:before {
+    -webkit-animation-delay: -1s;
+    animation-delay: -1s;
+}
+.sk-circle-bounce .sk-circle-4 {
+    transform: rotate(90deg);
+}
+.sk-circle-bounce .sk-circle-4:before {
+    -webkit-animation-delay: -0.9s;
+    animation-delay: -0.9s;
+}
+.sk-circle-bounce .sk-circle-5 {
+    transform: rotate(120deg);
+}
+.sk-circle-bounce .sk-circle-5:before {
+    -webkit-animation-delay: -0.8s;
+    animation-delay: -0.8s;
+}
+.sk-circle-bounce .sk-circle-6 {
+    transform: rotate(150deg);
+}
+.sk-circle-bounce .sk-circle-6:before {
+    -webkit-animation-delay: -0.7s;
+    animation-delay: -0.7s;
+}
+.sk-circle-bounce .sk-circle-7 {
+    transform: rotate(180deg);
+}
+.sk-circle-bounce .sk-circle-7:before {
+    -webkit-animation-delay: -0.6s;
+    animation-delay: -0.6s;
+}
+.sk-circle-bounce .sk-circle-8 {
+    transform: rotate(210deg);
+}
+.sk-circle-bounce .sk-circle-8:before {
+    -webkit-animation-delay: -0.5s;
+    animation-delay: -0.5s;
+}
+.sk-circle-bounce .sk-circle-9 {
+    transform: rotate(240deg);
+}
+.sk-circle-bounce .sk-circle-9:before {
+    -webkit-animation-delay: -0.4s;
+    animation-delay: -0.4s;
+}
+.sk-circle-bounce .sk-circle-10 {
+    transform: rotate(270deg);
+}
+.sk-circle-bounce .sk-circle-10:before {
+    -webkit-animation-delay: -0.3s;
+    animation-delay: -0.3s;
+}
+.sk-circle-bounce .sk-circle-11 {
+    transform: rotate(300deg);
+}
+.sk-circle-bounce .sk-circle-11:before {
+    -webkit-animation-delay: -0.2s;
+    animation-delay: -0.2s;
+}
+.sk-circle-bounce .sk-circle-12 {
+    transform: rotate(330deg);
+}
+.sk-circle-bounce .sk-circle-12:before {
+    -webkit-animation-delay: -0.1s;
+    animation-delay: -0.1s;
+}
+@keyframes sk-circle-bounce-delay {
+    0%, 80%, 100% {
+    transform: scale(0);
+    }
+    40% {
+    transform: scale(1.0);
+    }
+}
+    `;
+document.querySelector('section').insertAdjacentHTML('afterbegin', `
+<section class = 'loading' style='position:absolute;top: 50%;transform: translateY(-50%);'>
+<div class="sk-circle-bounce">
+    <div class="sk-child sk-circle-1"></div>
+    <div class="sk-child sk-circle-2"></div>
+    <div class="sk-child sk-circle-3"></div>
+    <div class="sk-child sk-circle-4"></div>
+    <div class="sk-child sk-circle-5"></div>
+    <div class="sk-child sk-circle-6"></div>
+    <div class="sk-child sk-circle-7"></div>
+    <div class="sk-child sk-circle-8"></div>
+    <div class="sk-child sk-circle-9"></div>
+    <div class="sk-child sk-circle-10"></div>
+    <div class="sk-child sk-circle-11"></div>
+    <div class="sk-child sk-circle-12"></div>
+</div>
+</section>
+`);
 
-    disableDropdowns = () => {
-        allDropdowns.forEach(item => {
-            item.style.display = 'none';
-        });
-    };
 
-    const createList = (countryName, countryCount, cities) => {
-        let citiesList = '';
-        cities.forEach(item => {
-            citiesList += `
+const disableButton = (event) => {
+    event.preventDefault();
+};
+const disableDropdowns = () => {
+    let start = 0;
+    let step;
+    if (Math.random()>0.5){
+        step = window.innerWidth*0.08;
+    } else {
+        step = -window.innerWidth*0.08;
+    }
+    allDropdowns.forEach(item => {
+        ;
+        if (item.style.display === 'block') {
+            item.style.cssText = `
+            max-height: 300px;
+            overflow: auto;
+            position: absolute;
+            background: white;
+            display: block`;
+            function moveDropdowns() {
+                let a = requestAnimationFrame(moveDropdowns);
+                item.style.left = start + 'px';
+                start += +step;
+                if (Math.abs(start) > window.innerWidth * 3 / 5) {
+                    cancelAnimationFrame(a);
+                    item.style.cssText = `display: none`;
+                }
+            };
+            moveDropdowns();
+        }
+    })
+
+};
+const createList = (countryName, countryCount, cities) => {
+    let citiesList = '';
+    cities.forEach(item => {
+        citiesList += `
             <div class="dropdown-lists__line">
             <div class="dropdown-lists__city">${item.name}</div>
             <div class="dropdown-lists__count">${item.count}</div>
             </div>
             `;
-        });
+    });
 
-        const list = `
+    const list = `
         <div class="dropdown-lists__countryBlock">
         <div class="dropdown-lists__total-line">
         <div class="dropdown-lists__country">${countryName}</div>
@@ -333,123 +492,135 @@ const label = document.querySelector('.label');
             ${citiesList}
         </div>
         `;
-        return list;
-    };
+    return list;
+};
+const createDefaultList = (data) => {
+    data["RU"].forEach(item => {
+        dropdownDefault.querySelector('.dropdown-lists__col').innerHTML += createList(item.country, item.count, item.cities);
+        totalLine = document.querySelectorAll('.dropdown-lists__total-line');
+    });
+}
+const eventListeners = (data) => {
+    button.href = '#';
+    button.style.cssText = 'cursor:context-menu;background: #00416A;';
+    button.addEventListener('click', disableButton);
 
-    const createDefaultList = (data) => {
-        data["RU"].forEach(item => {
-            dropdownDefault.querySelector('.dropdown-lists__col').innerHTML += createList(item.country, item.count, item.cities);
-            totalLine = document.querySelectorAll('.dropdown-lists__total-line');
-        });
-    }
-
-    createDefaultList(data);
-
-
-    const eventListeners = () => {
-        button.href = '#';
-        button.style.cssText = 'cursor:context-menu;background: #00416A;';
-        button.addEventListener('click', disableButton);
-
-        input.addEventListener('focus', () => {
+    input.addEventListener('focus', () => {
+        if (dropdownDefault.style.display !== 'block') {
             disableDropdowns();
-            dropdownDefault.style.display = 'block';
-        });
+        }
+        dropdownDefault.style.display = 'block';
+    });
 
 
-        input.addEventListener('input', () => {
-            disableDropdowns(); 
-            
-            if (input.value.trim() === '') {
-                button.href = '#';
-        button.style.cssText = 'cursor:context-menu;background: #00416A;';
-        button.addEventListener('click', disableButton);
-                label.style.opacity = 1;
-                dropdownDefault.style.display = 'block';
-            } else {
-                dropdownAutocomplete.style.display = 'block';
-                let overlaps = ``;
-                data["RU"].forEach(item => {
-                    item.cities.forEach(item => {
-                        if (item.name.toLowerCase().startsWith(input.value.toLowerCase().trim())) {
-                            overlaps += `
-                    <div class="dropdown-lists__line">
-                    <div class="dropdown-lists__city">${item.name}</div>
-                    <div class="dropdown-lists__count">${item.count}</div>
-                    </div>
-                    `;
-                        }
-                    });
-                });
-                if (overlaps === ''){
-                    dropdownAutocomplete.innerHTML = `<div class="dropdown-lists__countryBlock" style="text-align:center;color:red;">Ничего не найдено!</div>`;
-                } else{
-                dropdownAutocomplete.innerHTML = `<div class="dropdown-lists__countryBlock"></div>`;
-                dropdownAutocomplete.querySelector('.dropdown-lists__countryBlock').innerHTML = overlaps;
-                console.log(overlaps);}
-            }
-        });
+    input.addEventListener('input', () => {
+        disableDropdowns();
 
-
-        const lineListeners = (event) => {
-            if (event.target.closest('.dropdown-lists__list--default')) {
-                dropdownSelect.innerHTML = '';
-                const clone = event.target.parentNode.closest('.dropdown-lists__countryBlock').cloneNode(true);
-                dropdownSelect.append(clone);
-                disableDropdowns();
-                dropdownSelect.style.display = 'block';
-                totalLine = document.querySelectorAll('.dropdown-lists__total-line');
-                foo();
-            } else if (event.target.closest('.dropdown-lists__list--select')) {
-                disableDropdowns();
-                dropdownDefault.style.display = 'block';
-            }
-        };
-        function foo() {
-            totalLine.forEach(item => {
-                item.removeEventListener('click', lineListeners);
-                item.addEventListener('click', lineListeners);
-            });
-
-        };
-        foo();
-
-        document.addEventListener('click', event=>{
-            const target = event.target;
-            if (target.classList.contains('dropdown-lists__country') || target.classList.contains('dropdown-lists__city')){
-                label.style.opacity = 0;
-                
-                if (target.classList.contains('dropdown-lists__city')) {
-                    console.log(target);
-                    data["RU"].forEach(item => {
-                        item.cities.forEach(item => {
-                            if (item.name === target.textContent) {
-                                button.style.cssText = '';
-                                button.removeEventListener('click', disableButton);
-                                button.href = item.link;
-                            }
-                            console.log(item);
-                        });
-                    });
-                }
-                closeBtn.style.display = 'block';
-                input.value = target.textContent;
-            }
-        });
-
-        closeBtn.addEventListener('click', ()=>{
-            disableDropdowns();
+        if (input.value.trim() === '') {
             button.href = '#';
             button.style.cssText = 'cursor:context-menu;background: #00416A;';
             button.addEventListener('click', disableButton);
-            input.value = '';
             label.style.opacity = 1;
+            dropdownDefault.style.display = 'block';
+        } else {
+            dropdownAutocomplete.style.display = 'block';
+            let overlaps = ``;
+            data["RU"].forEach(item => {
+                item.cities.forEach(item => {
+                    if (item.name.toLowerCase().startsWith(input.value.toLowerCase().trim())) {
+                        overlaps += `
+                    <div class="dropdown-lists__line">
+                    <div class="dropdown-lists__city"><b>${item.name.substr(0, input.value.trim().length)}</b>${item.name.substr(input.value.trim().length)}</div>
+                    <div class="dropdown-lists__count">${item.count}</div>
+                    </div>
+                    `;
+                    }
+                });
+            });
+            if (overlaps === '') {
+                dropdownAutocomplete.innerHTML = `<div class="dropdown-lists__countryBlock" style="text-align:center;color:red;">Ничего не найдено!</div>`;
+            } else {
+                dropdownAutocomplete.innerHTML = `<div class="dropdown-lists__countryBlock"></div>`;
+                dropdownAutocomplete.querySelector('.dropdown-lists__countryBlock').innerHTML = overlaps;
+            }
+        }
+    });
+
+
+    const lineListeners = (event) => {
+        if (event.target.closest('.dropdown-lists__list--default')) {
+            dropdownSelect.innerHTML = '';
+            const clone = event.target.parentNode.closest('.dropdown-lists__countryBlock').cloneNode(true);
+            dropdownSelect.append(clone);
+            disableDropdowns();
+            dropdownSelect.style.display = 'block';
+            totalLine = document.querySelectorAll('.dropdown-lists__total-line');
+            foo();
+        } else if (event.target.closest('.dropdown-lists__list--select')) {
+            disableDropdowns();
+            dropdownDefault.style.display = 'block';
+        }
+    };
+    function foo() {
+        totalLine.forEach(item => {
+            item.removeEventListener('click', lineListeners);
+            item.addEventListener('click', lineListeners);
         });
 
+    };
+    foo();
 
-    }
+    document.addEventListener('click', event => {
+        const target = event.target;
+        if (target.classList.contains('dropdown-lists__country') || target.classList.contains('dropdown-lists__city')) {
+            label.style.opacity = 0;
 
-    eventListeners();
+            if (target.classList.contains('dropdown-lists__city')) {
+                data["RU"].forEach(item => {
+                    item.cities.forEach(item => {
+                        if (item.name === target.textContent) {
+                            button.style.cssText = '';
+                            button.removeEventListener('click', disableButton);
+                            button.href = item.link;
+                        }
+                    });
+                });
+            }
+            closeBtn.style.display = 'block';
+            input.value = target.textContent;
+        }
+    });
+
+    closeBtn.addEventListener('click', () => {
+        disableDropdowns();
+        button.href = '#';
+        button.style.cssText = 'cursor:context-menu;background: #00416A;';
+        button.addEventListener('click', disableButton);
+        input.value = '';
+        label.style.opacity = 1;
+        closeBtn.style.display = 'none';
+    });
+}
 
 
 
+
+const getData = () => {
+    fetch('db_cities.json').then((response) => {
+        if (response.status !== 200) {
+            throw new Error('Status is not 200!');
+        }
+        document.head.appendChild(style);
+        return (response.json());
+        ;
+    }).then((response) => {
+        setTimeout(() => {
+            document.head.removeChild(style);
+            document.querySelector('.loading').parentNode.removeChild(document.querySelector('.loading'));
+            document.querySelector('.input-cities').style.opacity = 1;
+            createDefaultList(response);
+            eventListeners(response);
+        }, 2000);
+    }).catch(error => console.error(error));
+}
+getData();
